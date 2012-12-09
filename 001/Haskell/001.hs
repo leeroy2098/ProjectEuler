8950c210918f8885557b52519debc31d7911f9c6
Simon L <simon@levermaenner.de>
2012-12-09 10:30:45 +0100

    More efficient now.

--- 001/Haskell/001.hs
+++ 001/Haskell/001.hs
@@ -1,8 +1,6 @@
 import Prelude hiding (sum)
 
-withFilter = sum $ filter(\ x -> x `mod` 3 == 0 || x `mod` 5 == 0) [1,2..999]
-
-withListComprehension = sum $ [x | x <- [1,2..999], x `mod` 3 == 0 || x `mod` 5 == 0]
+result = sum [3,6..999] + sum [5,10..999] - sum [15,30..999]
 
 -- No cheating, let's make our own sum function!
 -- Eta contraction
