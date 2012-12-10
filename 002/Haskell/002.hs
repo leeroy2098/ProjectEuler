9423193385cae33d8b507d9d60a129e6d7e37748
leeroy2098 <simon@levermaenner.de>
2012-12-10 06:57:16 +0100

    Update 002/Haskell/002.hs
    Fixed typo

--- 002/Haskell/002.hs
+++ 002/Haskell/002.hs
@@ -1,2 +1,2 @@
 fibs = scanl (+) 0 (1:fibs)
-result = foldr (+) 0 (takeWhile (\ x -> x < 4000000) (filter (\ x -> x `mod` 1 == 0)fibs))
+result = foldr (+) 0 (takeWhile (\ x -> x < 4000000) (filter (\ x -> x `mod` 2 == 0)fibs))
